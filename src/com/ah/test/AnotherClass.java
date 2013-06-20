fbed830cf34e1b89075ff9206030598b8638bd32
yfgong <yfgong@aerohive.com>
2013-06-20 17:50:49 +0800

    gyf commit and push 2#

--- src/com/ah/test/AnotherClass.java
+++ src/com/ah/test/AnotherClass.java
@@ -1,16 +1,13 @@
 package com.ah.test;
 
-public class TestClass {
+public class AnotherClass {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello!");
-		
-		System.out.println("gyf changed 1!");
-		
+		System.out.println("AnotherClass main");
 	}
 
 }
